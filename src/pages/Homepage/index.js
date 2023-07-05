00a5efcca495cabc05d1a02d36dd525ea53c53da
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-07-05 14:53:52 -0300

    fazendo o carrosel dos banners

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,16 +1,17 @@
-import Banner from "components/Banner/Banner";
+// import Banner from "components/Banner/Banner";
 import Categories from "components/Categories/Categories";
 import Features from "components/Features/Features";
 import Header from "components/Header";
 import Products from "components/Products/Products";
 import Footer from "components/Footer/Footer";
 import Newsletter from "components/Newsletter/Newsletter";
+import BannerCarousel from "components/Carousel/Carousel";
 
 function Homepage() {
     return(
      <>
         <Header />
-        <Banner />
+        <BannerCarousel />
         <Categories />
         <Products />
         <Features />
